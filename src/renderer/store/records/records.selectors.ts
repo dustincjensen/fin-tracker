7ab14a4c5f12359abfe2e23c62a9f7104224fd25
Dustin Jensen <dustincjensen@gmail.com>
2018-06-14 22:21:37 -0600

    Adds selector for ByAccountIdAndDate - Future account/year/month record selection will use this.

--- src/renderer/store/records/records.selectors.ts
+++ src/renderer/store/records/records.selectors.ts
@@ -1,8 +1,19 @@
 import IStore from '../store.interface';
+import { isInYearMonth } from '../../utils/date.util';
 
 export function ByAccountId(store: IStore, accountId: string) {
   if (accountId) {
     return store.records.filter(r => r.accountId === accountId);
   }
   return [];
-} 
+}
+
+export function ByAccountIdAndDate(store: IStore, accountId: string, date: string) {
+  if (accountId) {
+    return store.records.filter(r =>
+      r.accountId === accountId &&
+      isInYearMonth(r.date, date)
+    );
+  }
+  return [];
+}
