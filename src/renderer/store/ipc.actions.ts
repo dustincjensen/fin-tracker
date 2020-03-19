b39a6f82011591f8f02fab36871c4f11541a0bb3
Dustin Jensen <dustincjensen@gmail.com>
2020-03-19 00:39:10 -0600

    Comments and types on the IPC communication.

--- src/renderer/store/ipc.actions.ts
+++ src/renderer/store/ipc.actions.ts
@@ -1,3 +1,4 @@
+import { IpcRendererEvent } from 'electron';
 import * as pendingRecordsActions from './pending-records/pending-records.actions';
 import * as recordsActions from './records/records.actions';
 
@@ -6,6 +7,14 @@ const lookup: { [type: string]: Function } = {
   ['IPC_NEW_RECORDS_MERGED']: recordsActions.SaveNewRecords,
 };
 
-export function ipcHandler(_event, ipcType, ...args: unknown[]) {
+/**
+ * Handles the incoming communication from the main process.
+ * Looks up an action and triggers it based on the type from the background process.
+ * 
+ * @param _event    The renderer event.
+ * @param ipcType   The type to lookup to trigger the necessary action.
+ * @param args      The arguments to pass to the action.
+ */
+export function ipcHandler(_event: IpcRendererEvent, ipcType: string, ...args: unknown[]) {
   return lookup[ipcType](...args);
 }
--- src/renderer/store/ipc.send.ts
+++ src/renderer/store/ipc.send.ts
@@ -1,5 +1,13 @@
+import { Dispatch } from 'redux';
 import { send } from 'redux-electron-ipc';
 
-export function sender(dispatch, type, ...args: unknown[]) {
+/**
+ * Used by actions to send events to the background process for intensive calculations.
+ * 
+ * @param dispatch  The redux dispatch.
+ * @param type      The type of event that should be triggered in the background process.
+ * @param args      The arguments to pass to the background process.
+ */
+export function sender(dispatch: Dispatch, type: string, ...args: unknown[]) {
   dispatch(send('IPC_RECEIVE_RENDERER', type, ...args));
 }
