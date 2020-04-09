3c7b21000401fb847c6c4157c2f4a6281258fe96
Dustin Jensen <dustincjensen@gmail.com>
2020-04-09 00:08:09 -0600

    Add electron-store - Persist to file instead of local storage

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -10,12 +10,14 @@ import { rootReducer } from './store/store';
 import { IPersistedStore } from './store/store.interface';
 import './renderer.scss';
 
-// TODO change implementation of initial state.
-const initialState = JSON.parse(localStorage.getItem('reduxStore') || '{}');
-const store = createStore(rootReducer, initialState || {}, applyMiddleware(ipcReceive, toastMiddleware));
+const ElectronStore = require('electron-store');
+
+// TODO in future releases should add "value => JSON.stringify(value)" to reduce file size.
+const storage = new ElectronStore({ name: 'appState' });
+const store = createStore(rootReducer, storage.get('state') || {}, applyMiddleware(ipcReceive, toastMiddleware));
 
 store.subscribe(() => {
-  // TODO We shouldn't set local storage so quickly, subscribe gets called often.
+  // TODO should we have a delay before setting the state? Subscribe gets called often.
   const state = store.getState();
   const persistedState: IPersistedStore = {
     accounts: state.accounts,
@@ -23,8 +25,7 @@ store.subscribe(() => {
     categories: state.categories,
     records: state.records
   };
-  const serializedState = JSON.stringify(persistedState);
-  localStorage.setItem('reduxStore', serializedState);
+  storage.set('state', persistedState);
 });
 
 ReactDOM.render(
