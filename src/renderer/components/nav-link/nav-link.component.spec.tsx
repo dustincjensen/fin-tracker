352071e7ef848cce90779a2e6476c206bc82be2a
Dustin Jensen <dustincjensen@gmail.com>
2020-09-26 07:45:13 -0600

    Add jest and a few tests for proof of concept.

---
+++ src/renderer/components/nav-link/nav-link.component.spec.tsx
@@ -0,0 +1,13 @@
+import { shallow } from 'enzyme';
+import { Button } from 'evergreen-ui';
+import * as React from 'react';
+import { NavLink } from './nav-link.component';
+
+describe('components', () => {
+  describe('nav-link', () => {
+    it('should render a Button', () => {
+      const component = shallow(<NavLink to='/home' text='Home' iconBefore='home' />);
+      expect(component.find(Button).length).toBe(1);
+    });
+  });
+});
