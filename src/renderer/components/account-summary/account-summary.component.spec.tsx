a40f001cd9b2a717218d8b98dcf21a47a9469c38
Dustin Jensen <dustincjensen@gmail.com>
2020-10-02 15:09:08 -0600

    Add spec for account summary component

---
+++ src/renderer/components/account-summary/account-summary.component.spec.tsx
@@ -0,0 +1,30 @@
+import { shallow } from "enzyme";
+import * as React from 'react';
+import { AccountSummary } from "./account-summary.component";
+import { IAccountSummaryProps } from "./account-summary.props.interface";
+
+describe('components', () => {
+  describe('AccountSummary', () => {
+    let props: IAccountSummaryProps;
+
+    beforeEach(() => {
+      props = {
+        accountId: 'accountId',
+        balance: undefined,
+        dateOfLastTransaction: undefined,
+        iconName: 'bank-account',
+        name: 'Savings'
+      };
+    });
+
+    const assertRenderDetails = (balance: number, emptyAccountLength: number, accountDetailsLength: number) => {
+      const component = shallow(<AccountSummary {...props} balance={balance} />);
+      expect(component.find('[data-name="empty-account"]').length).toBe(emptyAccountLength);
+      expect(component.find('[data-name="account-details"]').length).toBe(accountDetailsLength);
+    };
+
+    it('should render empty account when balance undefined', () => assertRenderDetails(undefined, 1, 0));
+    it('should render empty account when balance null', () => assertRenderDetails(null, 1, 0));
+    it('should render account details when balance is provided', () => assertRenderDetails(1.23, 0, 1));
+  });
+});
